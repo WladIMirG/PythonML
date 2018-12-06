6fcb768e77ffb401ebe9ab6b586f588876ed887d
WladIMirG <ingwladimirgarces@gmail.com>
2018-12-06 06:49:35 -0500

    Arbol de desiciones con graficas

--- tree.py
+++ tree.py
@@ -34,7 +34,7 @@ print("Y_train:			", Y_train.shape)
 print("Y_test:				", Y_test.shape)
 
 """__________________"""
-arbol = DecisionTreeClassifier()
+arbol = DecisionTreeClassifier(max_depth=5)
 print("Entreno:			",arbol.fit(X_train, Y_train))
 print("Comprobacion1:		",arbol.score(X_test, Y_test))
 print("Comprobacion2:		",arbol.score(X_train, Y_train))
@@ -52,17 +52,45 @@ graphviz.Source(dot_graph).render ('arbol', view=True, format='png')
 # graph.render ('arbol', view=True, format='png')
 # graph.view()
 
-# import pydot 
-# import StringIO
-
-# dotfile = StringIO() 
-# tree.export_graphviz(dtreg, out_file=dotfile) 
-# pydot.graph_from_dot_data(dotfile.getvalue()).write_png("dtree2.png") 
-
+"""Esto si va en el programa"""
 caract = iris.data.shape[1]
 print(caract)
 plt.bar(range(caract), arbol.feature_importances_)
 plt.xticks(np.arange(caract), iris.feature_names)
 plt.xlabel("importancia de las caracteristicas")
 plt.ylabel("Caracteristica")
+
+
+n_classes = 3
+plot_colors = "bry"
+plot_step = 0.02
+
+for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3],
+								[1, 2], [1, 3], [2, 3]]):
+	X = iris.data[:, pair]
+	Y = iris.target
+	clf = DecisionTreeClassifier(max_depth = 3).fit(X, Y)
+
+	plt.subplot(2,3, pairidx + 1)
+
+	x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1 
+	y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+	xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+						 np.arange(y_min, y_max, plot_step))
+	Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+	Z = Z.reshape(xx.shape)
+	cs = plt.contourf(xx, yy, Z, cmap=plt.cm.Paired)
+
+	plt.xlabel(iris.feature_names[pair[0]])
+	plt.ylabel(iris.feature_names[pair[1]])
+	plt.axis("tight")
+
+	for i, color in zip(range(n_classes), plot_colors):
+		idx = np.where(Y == i)
+		plt.scatter(X[idx, 0], X[idx, 1], c=color, label=iris.target_names[i],
+					cmap=plt.cm.Paired)
+	plt.axis("tight")
+plt.suptitle("Ejemplos de clasificador de arboles")
+plt.legend()
 plt.show()
+
